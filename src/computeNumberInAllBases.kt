e538e980a447c26c7b519bcb77b1757bcbdc9623
Pierre MASSON <pierre.masson-ext@alstomgroup.com>
2024-09-19 14:53:44 +0000

    <Training Kotlin> commit code to get a number in base 10 from every other bases

---
+++ src/computeNumberInAllBases.kt
@@ -0,0 +1,17 @@
+import kotlin.math.pow
+
+fun convertNumberToBase(n:Int,base:Int):Int {
+    val str = n.toString().reversed()//reverse so the list will have correct index
+    var sum = 0
+    for (i in str.indices) {
+        val digit = str[i].toString().toInt() // Convert each character to digit
+        sum += digit * base.toDouble().pow(i.toDouble()).toInt() // Calculate the contribution to the sum
+    }
+    return sum
+}
+
+fun main(args: Array<String>) {
+    println(convertNumberToBase(101010,2))
+    println(convertNumberToBase(42,10))
+    println(convertNumberToBase(1120,3);splitInThreeSequence())
+}
